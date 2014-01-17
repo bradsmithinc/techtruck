671456960be2dc0d4ed0ba9444c7bf776acc94f3
Brad Smith <bradsmithinc@gmail.com>
2014-01-16 18:44:16 -0800

    sad

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-require "minitest/rails/railtie"
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
 Bundler.require(*Rails.groups(assets: %w(development test)))
